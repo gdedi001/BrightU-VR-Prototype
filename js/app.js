4bbb512e6119a6d487a07c5b20efd2b9ad042795
De diego, Gabriel X <gabriel.dediego@contractor.mybrighthouse.com>
2016-07-22 12:07:31 -0400

    add add access to entity elements

---
+++ js/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+//document.querySelector('#backgroundBHN').addEventListener('click', function(){
+//    this.setAttribute('material', 'color', 'red');
+//});
+//
+//document.querySelector('#security').addEventListener('click', function(){
+//    
+//});
+
+var brightHouse = document.getElementById("brightHouse"),
+    cloud = document.getElementById("cloud"),
+    internet = document.getElementById("internet"),
+    security = document.getElementById("security"),
+    firewall = document.getElementById("firewall");
+
+
+brightHouse.onclick = function(){
+    //this.setAttribute('position', '0.00 5.00 -5//');
+};
+
+
