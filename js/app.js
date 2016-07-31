913a9c44f9d6fdbd99bb8b8fe3adc5428e451c05
De diego, Gabriel X <gabriel.dediego@contractor.mybrighthouse.com>
2016-07-31 01:22:31 -0400

    clean up comments

--- js/app.js
+++ js/app.js
@@ -6,10 +6,6 @@ var brightHouse = document.querySelector("#brightHouse"),
     firewall = document.querySelector("#firewallHolder"),
     cloudExit = document.querySelector("#exit");
 
-//document.querySelector('#cloudShadow').addEventListener('click', function(){
-//    this.setAttribute('position', '0.00 5.00 -5.00');
-//    console.log('test complete');
-//});
 
 function cloudActivate(){
     document.querySelector('#cloud-yoyo').emit('yoyo');
@@ -21,7 +17,3 @@ function cloudActivate(){
 cloud.addEventListener('click', cloudActivate);
 cloudExit.addEventListener('click', cloudActivate);
 
-// Trigger an event to begin fading.
-//documet.querySelector('#test').addEventListener('click', function(){
-//    document.querySelector('#fading-cube').emit('fade');
-//});
